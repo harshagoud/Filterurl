8b23607e2254f28c8cb1e4dcc48618998fcdd761
harshagoud <73338829+harshagoud@users.noreply.github.com>
2023-04-25 12:40:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,12 +63,6 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-
-AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 300))
-AUTO_DELETE = environ.get('AUTO_DELETE', True)
-if AUTO_DELETE == "True":
-    AUTO_DELETE = True
-
 SHORTNER_SITE = environ.get('SHORTNER_SITE', 'omegalinks.in') 
 SHORTNER_API = environ.get('SHORTNER_API','c2d78a2519d484e84426c464b938353a0a9c6f8d')
 
